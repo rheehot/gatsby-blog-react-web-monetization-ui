2c6b3d04d4ef730b0869c816350474a3dcccc787
Eka <6597211+ekafyi@users.noreply.github.com>
2020-05-30 23:56:24 +0700

    update bio

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -4,17 +4,41 @@ import React from "react"
  * Change the content to add your own bio
  */
 
+const byline = (
+  <>
+    Test site to try the Web Monetization API by{" "}
+    <a href="https://dev.to/@ekafyi" rel="external">
+      @ekafyi
+    </a>
+    .
+  </>
+)
+const libs = (
+  <>
+    Uses{" "}
+    <a
+      href="https://github.com/ekafyi/react-web-monetization-ui"
+      rel="external"
+    >
+      react-web-monetization
+    </a>{" "}
+    and{" "}
+    <a
+      href="https://github.com/sharafian/react-web-monetization"
+      rel="external"
+    >
+      react-web-monetization-ui
+    </a>
+    .
+  </>
+)
+
 export default function Bio() {
   return (
     <>
-      Test site to try the Web Monetization API by{" "}
-      <a href="https://dev.to/@ekafyi" rel="external">
-        @ekafyi
-      </a>
-      . Photo by{" "}
-      <a href="https://unsplash.com/@micheile" rel="external">
-        Micheile Henderson
-      </a>
+      {byline}
+      <br />
+      {libs}
     </>
   )
 }
