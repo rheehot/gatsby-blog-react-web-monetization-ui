ab4740c54067c2b146403e0f9c78b74d205c6707
Eka <6597211+ekafyi@users.noreply.github.com>
2020-05-30 14:06:38 +0700

    use react-web-monetization instead of custom hook

--- src/components/exclusive.js
+++ src/components/exclusive.js
@@ -3,16 +3,17 @@
  * https://dev.to/emma/setting-up-web-monetization-on-your-gatsby-blog-with-rss-dgp
  */
 import React from "react"
-import { useMonetization } from "../hooks/use-monetization"
+import { useMonetizationState } from "react-web-monetization"
 
 const Exclusive = ({ children }) => {
-  const { isMonetized, isLoading } = useMonetization()
+  const monetization = useMonetizationState()
 
-  if (isLoading) {
+  if (monetization.state === "pending") {
     return <div>Loading...</div>
-  } else if (isMonetized) {
+  } else if (monetization.state === "started") {
     return <div>{children}</div>
   } else {
+    // monetization.state === 'stopped' || !monetization.state
     return <div>Monetized content is hidden.</div>
   }
 }
--- src/hooks/use-monetization.js
+++ src/hooks/use-monetization.js
@@ -2,41 +2,5 @@
  * useMonetization() hook
  * https://dev.to/emma/getting-started-with-web-monetization-in-react-3m4j
  */
-import { useEffect, useState } from "react"
 
-export const useMonetization = () => {
-  const [isMonetized, setIsMonetized] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    if (typeof document !== `undefined`) {
-      if (!document.monetization) {
-        // This means this user doesn't have monetization capabilities
-        // i.e. they don't have the Coil extension installed on their browser
-        setIsLoading(false)
-        setIsMonetized(false)
-        return
-      }
-
-      // Note: A user could have monetization capabilities (i.e. installed Coil)
-      // but that doesn't mean they've actually signed up for an account!
-      const { state } = document.monetization
-
-      // If the initial state is 'stopped', we can assume the user isn't
-      // going to pay, and so we can stop loading
-      if (state === "stopped") {
-        setIsLoading(false)
-        setIsMonetized(false)
-      }
-
-      // We add a listener to wait for the user to start paying
-      document.monetization.addEventListener("monetizationstart", () => {
-        console.log("🤑 monetization starts!")
-        setIsMonetized(true)
-        setIsLoading(false)
-      })
-    }
-  }, [])
-
-  return { isMonetized, isLoading }
-}
+// not used in this branch
